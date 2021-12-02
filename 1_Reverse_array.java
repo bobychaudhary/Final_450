dd14412ffe36b97d060c40ea386871439f5d1e56
bobychaudhary <bobychaudhary708@gmail.com>
2021-12-02 22:08:16 +0530

    Solution-1 Reverse array using two pointer  has been added

---
+++ 1_Reverse_array.java
@@ -0,0 +1,23 @@
+class Reverse{
+    public void reverse(int[] arr){
+        int start=0;;
+        int end=arr.length-1;
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,8,9,10,11};
+        Reverse obj=new Reverse();
+        obj.reverse(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+
